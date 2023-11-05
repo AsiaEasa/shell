303f75a29011a873cae367c1dd461c6b6b198276
AsiaEasa <asiaeasa89@gmail.com>
2023-11-05 06:29:14 -0800

    done

--- sh_2/fork.c
+++ sh_2/fork.c
@@ -11,23 +11,23 @@ int my_fork(char **arg)
 {
 	pid_t child_ID;
 	int ID_status;
+	if (access(arg[0], F_OK) != -1)
+	{ child_ID = fork();
 
-	child_ID = fork();
+		if (child_ID == -1)
+		{ free_all(arg);
+			perror("Error forking process");
+			exit(EXIT_FAILURE); }
 
-	if (child_ID == -1)
-	{ free_all(arg);
-		perror("Error forking process");
-		exit(EXIT_FAILURE); }
+		if (child_ID == 0)
+			if (execvp(arg[0], arg) == -1)
+				error(arg[0]);
 
-	if (child_ID == 0)
-	{if (execvp(arg[0], arg) == -1)
-		{ error(arg[0]); }
-		free (arg);
-		exit (127); }
 		if (waitpid(child_ID, &ID_status, 0) == -1)
 		{ free_all(arg);
 			perror("Command not found");
-			exit(EXIT_FAILURE);
+			exit(127);
 		}
-		return (-1);
-	}
+		return (-1); }
+	error(arg[0]);
+	exit(127); }

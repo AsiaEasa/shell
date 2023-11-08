47384b74df213dd3c0c403c5f6385c813101de27
AsiaEasa <asiaeasa89@gmail.com>
2023-11-07 23:17:05 -0800

    done

--- sh_2/fork.c
+++ sh_2/fork.c
@@ -20,12 +20,8 @@ int my_fork(char **arg)
 		exit(EXIT_FAILURE); }
 
 	if (child_ID == 0)
-	{	execvp(arg[0], arg);
-			free(arg);
-			if (errno == EACCES)
-				exit(127);
-			exit(2);
-	}
+		execvp(arg[0], arg);
+
 	if (waitpid(child_ID, &ID_status, 0) == -1)
 	{ free_all(arg);
 		perror("Command not found");

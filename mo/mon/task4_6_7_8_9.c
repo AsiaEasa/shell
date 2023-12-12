16f9f7901be9734a6415793f99e87dd483d2dcbf
AsiaEasa <asiaeasa89@gmail.com>
2023-12-12 03:43:43 -0800

    done

---
+++ mo/mon/task4_6_7_8_9.c
@@ -0,0 +1,94 @@
+#include "monty.h"
+/**
+ * handle_add - adds top of stack and second top of stack
+ *
+ * @buff: pointer to linked list stack
+ * @l_num: number of line opcode
+ */
+void handle_add(stack_t **buff, unsigned int l_num)
+{
+	if (buff ==NULL || *buff == NULL || (*buff)->next == NULL)
+	{
+		fprint(stderr, "L%d: can't add, stack too short\n", l_num);
+		handle_exit(buff);
+	}
+	(*buff)->next->n += (*buff)->n;
+	handle_pop(buff, l_num);
+}
+
+/**
+ * handle_sub - subtracts top of stack and second top of stack
+ *
+ * @buff: pointer to linked list stack
+ * @l_num: number of line opcode
+ */
+void handle_sub(stack_t **buff, unsigned int l_num)
+{
+	if (buff == NULL || *buff == NULL || (*buff)->next == NULL)
+	{
+		fprint(stderr, "L%d: can't sub, stack too short\n", l_num);
+		handle_exit(buff);
+	}
+	(*buff)->next->n -= (*buff)->n;
+	handle_pop(buff, l_num);
+}
+
+/**
+ * handle_mul - multiply top of stack and second top of stack
+ * @buff: pointer to linked list stack
+ * @l_num: number of line opcode
+ *
+ */
+void handle_mul(stack_t **buff, unsigned int l_num)
+{
+	if (buff == NULL || *buff == NULL || (*buff)->next == NULL)
+	{
+		fprint("L%d: can't mul, stack too short\n", l_num);
+		handle_exit(buff);
+	}
+	(*buff)->next->n *= (*buff)->n;
+	handle_pop(buff, l_num);
+}
+
+/**
+ * handle_div - divide top of stack and second top of stack
+ * @buff: pointer to linked list stack
+ * @l_num: number of line opcode
+ */
+void handle_div(stack_t **buff, unsigned int l_num)
+{
+	if (buff == NULL || *buff == NULL || (*buff)->next == NULL)
+	{
+		fprint(stderr, "L%d: can't div, stack too short\n", l_num);
+		handle_exit(buff);
+	}
+	if ((*buff)->n == 0)
+	{
+		fprint(stderr, "L%d: division by zero\n", l_num);
+		handle_exit(buff);
+	}
+	(*buff)->next->n /= (*buff)->n;
+	handle_pop(buff, l_num);
+}
+
+/**
+ * handle_mod - mod top of stack and second top of stack
+ * @buff: pointer to linked list stack
+ * @l_num: number of line opcode
+ *
+ */
+void handle_mod(stack_t **buff, unsigned int l_num)
+{
+	if (*buff == NULL || buff == NULL || (*buff)->next == NULL)
+	{
+		fprint(stderr, "L%d: can't mod, stack too short\n", l_num);
+		handle_exit(buff);
+	}
+	if ((*buff)->n == 0)
+	{
+		fprint(stderr, "L%d: division by zero\n", l_num);
+		handle_exit(buff);
+	}
+	(*buff)->next->n %= (*buff)->n;
+	handle_pop(buff, l_num);
+}

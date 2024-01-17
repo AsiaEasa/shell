d1001e204d360233ed214eb5804af69289c6f0da
AsiaEasa <asiaeasa89@gmail.com>
2024-01-17 05:08:01 -0800

    done

--- s/1-insertion_sort_list.c
+++ s/1-insertion_sort_list.c
@@ -1,48 +1,37 @@
 #include "sort.h"
 
 /**
- * swap - Swap two nodes in doubly-linked list.
- * @x: A pointer to the first node.
- * @y:  A pointer to the second node.
- */
-
-void swap(listint_t *x, listint_t *y)
-{
-	if (y->next)
-		y->next->prev = x;
-	if (x->prev)
-		x->prev->next = y;
-	x->next = y->next;
-	y->prev = x->prev;
-	x->prev = y;
-	y->next = x;
-}
-
-/**
  * insertion_sort_list - insertion to sort a doubly linked list
  *
- * @list: the address of the node (head)
+ * @list: the address of the node 
  *
  * Return: nothing (void)
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *x, *y;
+	listint_t *x, *y, *a;
 
 	if (!list || !*list || !(*list)->next)
 		return;
 	x = (*list)->next;
-	while (x)
+	while (x != NULL)
 	{
 		y = x;
 		x = x->next;
-		while (y && y->prev)
+		while (y != NULL && y->prev != NULL)
 		{
+			a = y->prev;
 			if (y->prev->n > y->n)
 			{
-				
-				swap(y->prev, y);
+				if (y->next)
+					y->next->prev = a;
+				if (a->prev)
+					a->prev->next = y;
+				a->next = y->next;
+				y->prev = a->prev;
+				a->prev = y;
+				y->next = a;
 				if (!y->prev)
 					*list = y;
 				print_list((const listint_t *)*list);
